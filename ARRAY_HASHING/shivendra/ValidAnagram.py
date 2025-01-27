4e7464c1ad62a5adcef61bf6271bb43057f038c4
sh1vendra <shivendra@txstate.edu>
2025-01-27 00:25:13 -0600

    Valid anagram lc easy

--- ARRAY_HASHING/shivendra/ValidAnagram.py
+++ ARRAY_HASHING/shivendra/ValidAnagram.py
@@ -2,4 +2,16 @@
 def isValidAnagram(s,t):
     if len(s) != len(t):
         return 
+    
+    dict1,dict2={},{}
+    
+    for num in s:
+        dict1[num] = 1+ dict1.get(num,0)
+    for num in t:
+        dict2[num] = 1+ dict2.get(num,0)
+    
+    return dict1==dict2
+
+print(isValidAnagram("woow","wwoo"))
+
 
