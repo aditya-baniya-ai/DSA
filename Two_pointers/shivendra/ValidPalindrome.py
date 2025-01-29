939328c77884f68afe14a6214b740bffd3d8adc9
sh1vendra <shivendra@txstate.edu>
2025-01-29 10:25:04 -0600

    lc easy string checking O(N)

---
+++ Two_pointers/shivendra/ValidPalindrome.py
@@ -0,0 +1,24 @@
+def validPalindrome(s):
+
+    l,r=0,len(s)-1
+
+    while l<r:
+
+        while l<r and not s[l].isalnum(): #donot forget checking bounds
+            l+=1
+
+        while r>l and not s[r].isalnum():
+            r-=1
+
+        if s[l].lower()!=s[r].lower(): #donot forget to lowercase letters
+            return False
+        
+        #Regular pointer sliding
+        l+=1
+        r-=1
+    # If not false, at the end of the loop-TRUE
+    return True
+
+
+print(validPalindrome("A man, a plan, a canal: Panama"))
+print(validPalindrome("race a car"))
