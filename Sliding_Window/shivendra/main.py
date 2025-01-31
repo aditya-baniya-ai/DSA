4e4e8549548ea35b459a3badf77c23724c701651
sh1vendra <shivendra@txstate.edu>
2025-01-31 13:41:05 -0600

    Notes for sliding window

---
+++ Sliding_Window/shivendra/main.py
@@ -0,0 +1,4 @@
+#always check if window is valid and then proceed accordingly
+#Rule1: len(window) = (r - l) + 1 
+#Rule2: use longest = max(longest, len of window: (r-l) + 1
+#Rule3: use hashmap{} or set(), sett.add(), sett.remove()
