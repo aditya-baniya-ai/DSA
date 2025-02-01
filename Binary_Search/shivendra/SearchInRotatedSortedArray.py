5e3bc1f57800e8153a37a3fc8848079bf5384cf3
sh1vendra <shivendra@txstate.edu>
2025-02-01 15:04:36 -0600

    O(logN) soln using precise BS

---
+++ Binary_Search/shivendra/SearchInRotatedSortedArray.py
@@ -0,0 +1,24 @@
+def search(nums,target):
+        
+    l,r = 0, len(nums)-1
+
+    while l<=r: #=
+        m = (l+r)//2
+        if target == nums[m]:
+            return m
+        #we are in L array and wanna go to R array
+        if nums[m] >= nums[l]:#=
+            if target > nums[m] or target < nums[l]:
+                #simple bs logic or sorted array logic
+                l = m+1
+            else: 
+                r = m-1
+        #We are in R array and wanna go to L array
+        else:
+            if target < nums[m] or target > nums[r]:
+                r = m-1
+            else:
+                l = m+1
+    return -1 
+
+print(search([4,5,6,7,0,1,2], 0))
