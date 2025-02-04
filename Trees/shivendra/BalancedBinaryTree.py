79a71604faa900e247f7d37fc76965af55eb596d
sh1vendra <shivendra@txstate.edu>
2025-02-04 02:01:22 -0600

    dfs and height diff check

---
+++ Trees/shivendra/BalancedBinaryTree.py
@@ -0,0 +1,26 @@
+from typing import Optional
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+class Solution:  
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        balanced = [True] #list of boolean set to default: True
+
+        def dfs(root):
+            if not root:
+                return 0
+            left_height = dfs(root.left)
+            right_height = dfs(root.right)
+
+            if abs(left_height - right_height) > 1: #Unbalanced found, false and exit
+                balanced[0] = False 
+                return 0 #ends the program
+
+            return max(left_height, right_height) +1 #height of the current node
+
+        dfs(root) #Start DFS traversal
+        return balanced[0]
