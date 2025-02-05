3153e8630cb03bb87f76adf4cacd6a14c6085c28
sh1vendra <shivendra@txstate.edu>
2025-02-04 22:18:40 -0600

    Notes

---
+++ Trees/shivendra/main.py
@@ -0,0 +1,15 @@
+'''
+from collections import deque
+d = deque()
+
+Stack: d.append(), d.pop()
+
+Queue: d.append(), d.popleft()
+
+List: d.append(), d.pop()
+
+sett = set()
+sett.add(), sett.remove()
+
+'''
+
