75a2f2639abb85a55403ee75d496487c029d84f6
sh1vendra <shivendra@txstate.edu>
2025-02-09 12:41:13 -0600

    Heap Notes

--- Heap_PriorityQueue/shivendra/main.py
+++ Heap_PriorityQueue/shivendra/main.py
@@ -1,6 +1,6 @@
 '''
 to convert into maxHeap:
-    maxHeap = {- num for num in <list>}
+    maxHeap = [- num for num in <list>] : [] NOT {} as {}=set, and []= list
 to covert the answer back to positive value:
-    abs(ans)
+    abs(ans) : DONOT forget to make it back positive using abs
 '''
