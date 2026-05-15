f1eecb1bf7b758772b7b89d13353d33de2752d4b
sh1vendra <shivendra@txstate.edu>
2026-05-14 20:17:47 -0500

    simplify topk frequent and add notes

--- ARRAY_HASHING/shivendra/Topk3.py
+++ ARRAY_HASHING/shivendra/Topk3.py
@@ -1,10 +1,13 @@
-class Solution:
-    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d = {}
-        for num in nums:
-            d[num] = 1 + d.get(num, 0)
-        sortedList = sorted(d, key=d.get, reverse=True)
-        return sortedList[:k]
+
+def topKFrequent(nums, k):
+    d = {}
+    for num in nums:
+        d[num] = 1 + d.get(num, 0)
+    sorted_key = sorted(d, key=d.get, reverse=True)
+# or sorted_key = sorted_key[::-1]
+    return sorted_key[:k] #returns 0,1 indices if k = 2
 
 # Time: O(n log n), not optimal but easy and intuitive
 # Space: O(n), works for interviews
+
+        
